71eaa72ab44213ec93b15e412af761ce3965bfaa
Eliott BRICOUT <eliott.bricout@etudiant.univ-lille1.fr>
2015-01-28 10:10:25 +0100

    il reponds enfin repete

--- webserver/main.c
+++ webserver/main.c
@@ -4,26 +4,28 @@
 #include <unistd.h>
 
 #define SIZE_BUFF 2048
-#define char buff[SIZE_BUFF];
+char buff[SIZE_BUFF];
 
 int main ( int argc , char **argv ){
 /* Arnold Robbins in the LJ of February '95 , describing RCS */
 	int socket_client;
+	int ret;
 	if(argc>1 && strcmp(argv[1], "-advice" ) == 0) {
 		printf (" Don 't Panic !\n " );
 		return 42;
 	}
 	socket_client=creer_serveur(8080);
 	
-	while(1){
+
 	
-		while(read(socket_client ,buff,SIZE_BUFF)>0){
-			write ( socket_client , buff , strlen (buff));
-		}
-	
+	while((ret=read(socket_client ,buff,SIZE_BUFF))>0){
+		if(write ( socket_client , buff , ret)==-1)
+			perror("probleme de write");
 	}
 	
-	printf ( " Need an advice ?\n " );
+	
+	
+	printf ( " un client a quitter le serveur car il etait nul\n " );
 	return 0;
 	
 }

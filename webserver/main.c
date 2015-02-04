1fb52cb302588d6b68237f0e79daf91fda0af4de
jeromgsq <jerome.ghesquiere@gmail.com>
2015-02-04 08:53:14 +0100

    Indentation

--- webserver/main.c
+++ webserver/main.c
@@ -13,49 +13,57 @@
 
 #define SIZE_BUFF 2048
 char buff[SIZE_BUFF];
-int nbclient=0;
+int nbclient = 0;
 
-int main ( int argc , char **argv ){
-/* Arnold Robbins in the LJ of February '95 , describing RCS */
+int main ( int argc , char **argv )
+{
 	int socket_serveur;
 	int socket_client;
-	
+
 	initSignaux();
-	
-	if(argc>1 && strcmp(argv[1], "-advice" ) == 0) {
+	if (argc > 1 && strcmp(argv[1], "-advice" ) == 0)
+	{
 		printf (" Don 't Panic !\n " );
 		return 42;
 	}
-	socket_serveur=creer_serveur(8080);
-	
-	while(1){
+
+	socket_serveur = creer_serveur(8080);
+
+	while (1)
+	{
 		socket_client = accept ( socket_serveur , NULL , NULL );
-		if ( socket_client == -1){
+		if ( socket_client == -1)
+		{
 			perror ( "bug de connection" );
-		}else{
-		nbclient++;
-			if(fork()==0){
-				printf ( " un client viens de ce connecter\n " );
-				
-				printf ( " vous avez %d client connecter \n ",nbclient );
-				
-				gestion_client(socket_client);
-			}
 		}
-	}
-	
-	return 0;
-	
+		else
+		{
+			nbclient++;
+			if (fork() == 0)
+			{
+              printf ( " un client viens de ce connecter\n " );
+
+              printf ( " vous avez %d client connecter \n ", nbclient );
+
+              gestion_client(socket_client);
+          }
+      }
+  }
+
+  return 0;
+
 }
 
-void gestion_client(int socket_client){
+void gestion_client(int socket_client)
+{
 	/* On peut maintenant dialoguer avec le client */
-	const char * message_bienvenue = " Bonjour , bienvenue sur mon serveur bitch \n " ;
+	const char *message_bienvenue = " Bonjour , bienvenue sur mon serveur bitch \n " ;
 	int ret;
 	write ( socket_client , message_bienvenue , strlen ( message_bienvenue ));
-	
-	while((ret=read(socket_client ,buff,SIZE_BUFF))>0){
-		if(write ( socket_client , buff , ret)==-1)
+
+	while ((ret = read(socket_client , buff, SIZE_BUFF)) > 0)
+	{
+		if (write ( socket_client , buff , ret) == -1)
 			perror("probleme de write");
 	}
 
@@ -63,20 +71,22 @@ void gestion_client(int socket_client){
 	exit(0);
 }
 
-void initSignaux(){
-	if(signal(SIGPIPE,SIG_IGN)==SIG_ERR)
+void initSignaux()
+{
+	if (signal(SIGPIPE, SIG_IGN) == SIG_ERR)
 		perror("signal");
 
 	struct sigaction sa;
 	sa.sa_handler = traitement_signal;
 	sigemptyset (&sa.sa_mask);
 	sa.sa_flags = SA_RESTART;
-	
-	if(sigaction(SIGCHLD,&sa,NULL)==-1)
+
+	if (sigaction(SIGCHLD, &sa, NULL) == -1)
 		perror("sigaction(SIGCHLD)");
-		
+
 }
 
-void traitement_signal(int sig){
+void traitement_signal(int sig)
+{
 	wait(NULL);
 }
--- webserver/socket.c
+++ webserver/socket.c
@@ -6,36 +6,39 @@
 #include "socket.h"
 #include <string.h>
 
-int creer_serveur (int port ){
+int creer_serveur (int port )
+{
 
-	int socket_serveur ;
-	struct sockaddr_in saddr ;
-	
+    int socket_serveur ;
+    struct sockaddr_in saddr ;
 
-	socket_serveur = socket( AF_INET , SOCK_STREAM , 0);
-	if ( socket_serveur == -1){
-		perror ( "socket_serveur" );
-	}
-	/* Utilisation de la socket serveur */
-	
-	int optval = 1;
-	if ( setsockopt ( socket_serveur , SOL_SOCKET , SO_REUSEADDR , & optval , sizeof(int)) == -1)
-		perror ( " Can not set SO_REUSEADDR option " );
-	
-	
-	saddr.sin_family = AF_INET ; /* Socket ipv4 */
-	saddr.sin_port = htons (port); /* Port d ’ écoute */
-	saddr.sin_addr.s_addr = INADDR_ANY ; 
-	/* écoute sur toutes les interfaces */
-	
-	if(bind(socket_serveur, (struct sockaddr *) &saddr,sizeof(saddr))==-1){
-		perror ( " bind socker_serveur " );
-	}
-	
-	
-	if ( listen ( socket_serveur , 10) == -1){
-		perror ( " listen socket_serveur " );
-	}
+    socket_serveur = socket( AF_INET , SOCK_STREAM , 0);
+    if ( socket_serveur == -1)
+    {
+        perror ( "socket_serveur" );
+    }
+    /* Utilisation de la socket serveur */
 
-	return socket_serveur;
+    int optval = 1;
+    if ( setsockopt ( socket_serveur , SOL_SOCKET , SO_REUSEADDR , & optval , sizeof(int)) == -1)
+        perror ( " Can not set SO_REUSEADDR option " );
+
+
+    saddr.sin_family = AF_INET ; /* Socket ipv4 */
+    saddr.sin_port = htons (port); /* Port d ’ écoute */
+    saddr.sin_addr.s_addr = INADDR_ANY ;
+    /* écoute sur toutes les interfaces */
+
+    if (bind(socket_serveur, (struct sockaddr *) &saddr, sizeof(saddr)) == -1)
+    {
+        perror ( " bind socker_serveur " );
+    }
+
+
+    if ( listen ( socket_serveur , 10) == -1)
+    {
+        perror ( " listen socket_serveur " );
+    }
+
+    return socket_serveur;
 }
